662499fe158bde83959af02ec917eab84754bf53
Farhad Eidi <farcom7@gmail.com>
2025-01-10 20:58:51 +0300

    feat: add turbogen generator

--- packages/unative/src/components/ui/box/web.tsx
+++ packages/unative/src/components/ui/box/web.tsx
@@ -3,26 +3,51 @@ import React from "react";
 import { cn } from "../../../utils";
 
 export type BoxProps = React.HtmlHTMLAttributes<HTMLDivElement>;
-export const Box = ({ ...props }: BoxProps) => {
-  return <div suppressHydrationWarning {...props} />;
-};
+export const Box = React.forwardRef<HTMLDivElement, BoxProps>(
+  ({ ...props }, ref) => {
+    return <div ref={ref} suppressHydrationWarning {...props} />;
+  },
+);
+Box.displayName = "Box";
 
-export const HStack = ({ className, ...props }: BoxProps) => {
-  return <Box className={cn("flex flex-row gap-1", className)} {...props} />;
-};
+export const HStack = React.forwardRef<HTMLDivElement, BoxProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <Box
+        ref={ref}
+        className={cn("flex flex-row gap-1", className)}
+        {...props}
+      />
+    );
+  },
+);
+HStack.displayName = "HStack";
 
-export const VStack = ({ className, ...props }: BoxProps) => {
-  return <Box className={cn("flex flex-col gap-1", className)} {...props} />;
-};
+export const VStack = React.forwardRef<HTMLDivElement, BoxProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <Box
+        ref={ref}
+        className={cn("flex flex-col gap-1", className)}
+        {...props}
+      />
+    );
+  },
+);
+VStack.displayName = "VStack";
 
-export const Center = ({ className, ...props }: BoxProps) => {
-  return (
-    <Box
-      className={cn(
-        "flex flex-col items-center justify-center gap-1",
-        className,
-      )}
-      {...props}
-    />
-  );
-};
+export const Center = React.forwardRef<HTMLDivElement, BoxProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <Box
+        ref={ref}
+        className={cn(
+          "flex flex-col items-center justify-center gap-1",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+Center.displayName = "Center";
